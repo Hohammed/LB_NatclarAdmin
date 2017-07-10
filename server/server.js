83754f8d495085040bb6cfcaae18612425970dc0
Hohammed Maximo Estrada Vargas <hestrada@SGNATCLAR.LOCAL>
2017-07-10 10:12:48 -0500

    M

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
+// Probando el controlm de versiones
 boot(app, __dirname, function(err) {
   if (err) throw err;
 
