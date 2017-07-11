8ae3ffa20697e88ebcb7b3a0e32c21f3aa15eaf8
Hohammed <hohammed@gmail.com>
2017-07-11 13:45:25 -0500

    Desde Sublime

--- server/server.js
+++ server/server.js
@@ -19,19 +19,20 @@ app.start = function() {
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
 // Probando el controlm de versiones 
 // Otra lirna
 // Nureva Bersion del codigo
-//Una nueva version 
-//DESDE SUBLIME
-//des sublime
-//Desde Sublome 
+//Una nueva version ASASAS
+//DESDE SUBLIMEASASAS
+//des sublimeAS
+//Desde SublomeAS
+//DES DE SUBLIME 
 
 boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
+  //SOPNI
   if (require.main === module)
     app.start();
 });
